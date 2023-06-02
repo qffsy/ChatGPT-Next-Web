497867853c3bb04f66dc842b5b4aadfac1fa9dfc
xiangjun <68806314+qffsy@users.noreply.github.com>
2023-06-02 14:28:05 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,7 +437,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>超级GPT-嗨次元-hicy.cn</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
