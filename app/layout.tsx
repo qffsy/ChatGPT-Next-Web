2aa0b2c67bb8c42b88b320bb52474a4054601b42
xiangjun <68806314+qffsy@users.noreply.github.com>
2023-06-02 14:30:30 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "超级GPT-嗨次元-gpt.hicy.cn",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "超级GPT-嗨次元-gpt.hicy.cn2",
     statusBarStyle: "default",
   },
 };
